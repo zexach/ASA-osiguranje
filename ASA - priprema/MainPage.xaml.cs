bb50c0d2b72b2e1cc220b6eef3d7d11793dc245b
Emir Zambakovic <83666062+zexach@users.noreply.github.com>
2022-12-09 15:21:17 +0100

    Added functionalities

--- ASA - priprema/MainPage.xaml.cs
+++ ASA - priprema/MainPage.xaml.cs
@@ -55,5 +55,80 @@ public partial class MainPage : ContentPage
 
     }
 
+    private void kalkulacija(object o, EventArgs e)
+    {
+        bool osi1 = osiguranje1Checkbox.IsChecked;
+        bool osi2 = osiguranje2Checkbox.IsChecked;
+        bool osi3 = osiguranje3Checkbox.IsChecked;
+        bool osi4 = osiguranje4Checkbox.IsChecked;
+        bool osi5 = osiguranje5Checkbox.IsChecked;
+        bool osi6 = osiguranje6Checkbox.IsChecked;
+
+        int osi1Kolicina, osi2Kolicina, osi3Kolicina, osi4Kolicina, osi5Kolicina, osi6Kolicina;
+
+        int.TryParse(osiguranje1Kolicina.Text, out osi1Kolicina);
+        int.TryParse(osiguranje2Kolicina.Text, out osi2Kolicina);
+        int.TryParse(osiguranje3Kolicina.Text, out osi3Kolicina);
+        int.TryParse(osiguranje4Kolicina.Text, out osi4Kolicina);
+        int.TryParse(osiguranje5Kolicina.Text, out osi5Kolicina);
+        int.TryParse(osiguranje6Kolicina.Text, out osi6Kolicina);
+
+        double ukupnaCijena = 0;
+        double osiguranaSuma = 0;
+
+        int klasaIndex = klasaPicker.SelectedIndex;
+
+        int klasaCijena = 0;
+        int klasaOsiguraneSume = 0;
+
+        if(klasaIndex >= 0)
+        {
+            klasaCijena = listaKlasaOsiguranja[klasaIndex].faktorCijene;
+            klasaOsiguraneSume = listaKlasaOsiguranja[klasaIndex].faktorOsiguraneSume;
+        }
+
+
+        ukupnaCijena += (listaVrstaOsiguranja[0].cijena * osi1Kolicina) + (listaVrstaOsiguranja[1].cijena * osi2Kolicina) + (listaVrstaOsiguranja[2].cijena * osi3Kolicina) +
+            (listaVrstaOsiguranja[3].cijena * osi4Kolicina) + (listaVrstaOsiguranja[4].cijena * osi5Kolicina) + (listaVrstaOsiguranja[5].cijena * osi6Kolicina) + klasaCijena;
+
+        osiguranaSuma += (listaVrstaOsiguranja[0].osiguranaSumaPoUsluzi * osi1Kolicina) + (listaVrstaOsiguranja[1].osiguranaSumaPoUsluzi * osi2Kolicina) +
+            (listaVrstaOsiguranja[2].osiguranaSumaPoUsluzi * osi3Kolicina) + (listaVrstaOsiguranja[3].osiguranaSumaPoUsluzi * osi4Kolicina) +
+            (listaVrstaOsiguranja[4].osiguranaSumaPoUsluzi * osi5Kolicina) + (listaVrstaOsiguranja[5].osiguranaSumaPoUsluzi * osi6Kolicina) + klasaOsiguraneSume;
+
+        bool porodicniPaket = switchPorodicno.IsToggled;
+
+        if (porodicniPaket) ukupnaCijena *= 1.3;
+
+        int brVrsta = 0;
+
+        if (osi1) brVrsta++;
+        if (osi2) brVrsta++;
+        if (osi3) brVrsta++;
+        if (osi4) brVrsta++;
+        if (osi5) brVrsta++;
+        if (osi6) brVrsta++;
+
+
+        if (!osi1 && !osi2 && !osi3 && !osi4 && !osi5 && !osi6)
+        {
+            DisplayAlert("Greska", "Niste izabrali nijednu vrstu osiguranja", "Nazad");
+        }else if( (osi1 && osi1Kolicina == 0) || (osi2 && osi2Kolicina == 0) || (osi3 && osi3Kolicina == 0) ||
+            (osi4 && osi4Kolicina == 0) || (osi5 && osi5Kolicina == 0) || (osi6 && osi6Kolicina == 0))
+        {
+            DisplayAlert("Greska", "Niste unijeli količinu Za odabranu vrstu osiguranja", "Nazad");
+        }else if(klasaIndex < 0)
+        {
+            DisplayAlert("Greska", "Niste unijeli klasu osiguranja", "Nazad");
+        }
+        else DisplayAlert("POLICA OSIGURANJA", 
+            "Ukupna vrijednost osiguranja: " + ukupnaCijena.ToString() + " KM" +
+            "\nBroj odabranih vrsta osiguranja: " + brVrsta + 
+            "\nOsigurana suma: " + osiguranaSuma + " KM" + 
+            "\nPorodični paket: " + porodicniPaket.ToString() + 
+            "\nKlasa index: " + klasaIndex, 
+            "Ok");
+
+    }
+
 }
 
